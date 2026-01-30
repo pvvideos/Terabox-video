b90c3cf92035199fa020b1116a96534ba9d314e2
pvvideos <provider04officiall@gmail.com>
2026-01-30 17:15:00 +0530

    Update data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- data.js
+++ data.js
@@ -1,16 +1,16 @@
 const posters = [
   {
     id: 1,
-    title: "Movie One",
-    image: "images/poster1.jpg",
-    terabox: "https://terabox-link-1",
-    telegram: "https://t.me/yourchannel"
+    title: "Chashmish viral girl ",
+    image: "IMG_20260130_165312_974.jpg",
+    terabox: "https://1024terabox.com/s/1DUlspvTp2HZCayEj-kh9hQ",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
   },
   {
     id: 2,
-    title: "Movie Two",
+    title: "19 minnitus viral video",
     image: "images/poster2.jpg",
-    terabox: "https://terabox-link-2",
-    telegram: "https://t.me/yourchannel"
+    terabox: "https://teraboxurl.com/s/12LEPgspYBPW8cZ8_os81xg",
+    telegram: "https://earning-boot-provider-04.in/movies-here"
   }
 ];
