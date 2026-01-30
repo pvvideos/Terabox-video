29c2b82812ea4dc91191307fba2300041ebbf12a
pvvideos <provider04officiall@gmail.com>
2026-01-30 20:28:43 +0530

    Update data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- data.js
+++ data.js
@@ -1,18 +1,10 @@
 const posters = [
   {
     id: 1,
-    title: "instgram chashmish viral girl video",
+    title: "Test Movie One",
     image: "images/IMG_20260130_165312_974.jpg",
-    description: " most trading viral Instagram video chashmish girl is here click here to watch full videos",
-    terabox: "https://1024terabox.com/s/1DUlspvTp2HZCayEj-kh9hQ",
-    telegram: "https://earning-boot-provider-04.in/movies-here"
-  },
-  {
-    id: 2,
-    title: " 19 minit 34 second viral video full hd ",
-    image: "images/IMG_20260130_171412_225.jpg"",
-    description: "Instagram most viral video 19:34 second a couple enjoying ",
-    terabox: "https://teraboxurl.com/s/12LEPgspYBPW8cZ8_os81xg",
-    telegram: "https://earning-boot-provider-04.in/movies-here"
+    description: "Test description",
+    terabox: "https://example.com",
+    telegram: "https://t.me/example"
   }
 ];
