3adaaa302afa29cfa23e9f76f4c9865f50a84741
pvvideos <provider04officiall@gmail.com>
2026-01-30 23:46:03 +0530

    Update data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- data.js
+++ data.js
@@ -2,7 +2,7 @@ const posters = [
   {
     id: 1,
     title: "Instagram chashmish viral girl video",
-    image: "IMG_20260130_233522.jpg",
+    image: "images/IMG_20260130_233522.jpg",
     description: "Most trending Instagram viral video",
     terabox: "https://1024terabox.com/s/1DUlspvTp2HZCayEj-kh9hQ",
     telegram: "https://earning-boot-provider-04.in/movies-here"
@@ -18,7 +18,7 @@ const posters = [
   {
     id: 3,
     title: "fatima jatoi viral Instagram most viral video hd ",
-    image: "ChatGPT Image Jan 30, 2026, 09_33_30 PM.png",
+    image: "images/ChatGPT Image Jan 30, 2026, 09_33_30 PM.png",
     description: "fatima jatoi Pakistani viral Instagram video hd",
     terabox: "https://1024terabox.com/s/187vXBKbj8zZKqaXdGsFK-A",
     telegram: "https://earning-boot-provider-04.in/movies-here"
