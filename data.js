4e57a410c0ac3e3e2f09ddd08ab50b6fe6047605
pvvideos <provider04officiall@gmail.com>
2026-01-30 16:46:38 +0530

    Create data.js
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

---
+++ data.js
@@ -0,0 +1,16 @@
+const posters = [
+  {
+    id: 1,
+    title: "Movie One",
+    image: "images/poster1.jpg",
+    terabox: "https://terabox-link-1",
+    telegram: "https://t.me/yourchannel"
+  },
+  {
+    id: 2,
+    title: "Movie Two",
+    image: "images/poster2.jpg",
+    terabox: "https://terabox-link-2",
+    telegram: "https://t.me/yourchannel"
+  }
+];
