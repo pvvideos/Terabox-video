c233fdbc232a6636c786c8823a4a1955ccb2603b
pvvideos <provider04officiall@gmail.com>
2026-03-17 18:20:36 +0530

    Update index.php
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 
+<?php
+include "db.php";
+include "includes/header.php";
+?>
+
+<h2>Welcome to Provider Hub</h2>
+<p>Short links and earn from traffic.</p>
+
+<?php if(isset($_SESSION['user_id'])): ?>
+    <p><a href="dashboard.php">Go to Dashboard</a></p>
+    <p><a href="logout.php">Logout</a></p>
+<?php else: ?>
+    <p><a href="signup.php">Signup</a> | <a href="login.php">Login</a></p>
+<?php endif; ?>
+
+<?php include "includes/footer.php"; ?>
