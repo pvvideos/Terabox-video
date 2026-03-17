ea6a5b632b1978dc72bfa6473a4f1db4aff247a7
pvvideos <provider04officiall@gmail.com>
2026-03-17 18:17:56 +0530

    Create header.php
    Signed-off-by: pvvideos <provider04officiall@gmail.com>

---
+++ includes/header.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Provider Hub</title>
+    <link rel="stylesheet" href="/assets/style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Provider Hub</h1>
